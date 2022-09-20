c5c3b6ac5e590544c233ecb971f263b303bf8f0d
Christer <au644764@uni.au.dk>
2022-09-20 19:14:04 +0200

    meget fikset. check test

---
+++ Project2/Testing.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from Graham_scan import grahams_scan
+from Gift_wrapping import gift_wrapping
+from Shared import Point, print_points
+
+
+x = np.random.randint(0,10,50)
+y = np.random.randint(0,10,50)
+
+
+
+points = []
+for i in range(50):
+    p = Point(x[i],y[i])
+    points.append(p)
+    del p
+
+upper_hull = gift_wrapping(points)
+print_points(upper_hull)
+
+a = []
+b = []
+for i in range(len(upper_hull)):
+    a.append(upper_hull[i].x)
+    b.append(upper_hull[i].y)
+    
+
+
+
+plt.scatter(x,y) 
+plt.plot(a, b, color='red', linestyle='dashed', marker='o')
+plt.show()
