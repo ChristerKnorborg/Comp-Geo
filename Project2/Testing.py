e6610e3350b7697ff3de0ad84ff575de36869611
Christer <au644764@uni.au.dk>
2022-10-04 09:57:20 +0200

    added testing

--- Project2/Testing.py
+++ Project2/Testing.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import matplotlib.pyplot as plt
 import numpy as np
 from Graham_scan import grahams_scan
@@ -7,40 +8,80 @@ from Chan_algorithm import chan_algorithm
 
 
 
-def square_test(algorithm, num_of_points, range_lower, range_upper):
+def square_test(num_of_points, range_lower, range_upper):
     
 
     x = np.random.randint(range_lower,range_upper,num_of_points)
     y = np.random.randint(range_lower,range_upper,num_of_points)
 
-
-
-    points = []
+    graham_points = []
     for i in range(num_of_points):
         p = Point(x[i],y[i])
-        points.append(p)
+        graham_points.append(p)
         del p
 
-    upper_hull = algorithm(points)
+    gift_points = deepcopy(graham_points)
+    chan_points = deepcopy(graham_points)
+
+
+    graham_upper_hull = grahams_scan(graham_points)
+    gift_upper_hull = gift_wrapping(gift_points)
+    chan_upper_hull = chan_algorithm(chan_points)
 
 
 
-    print_points(upper_hull)
+    #print_points(upper_hull)
 
-    a = []
-    b = []
-    for i in range(len(upper_hull)):
-        a.append(upper_hull[i].x)
-        b.append(upper_hull[i].y)
+    graham_a = []
+    graham_b = []
+    for i in range(len(graham_upper_hull)):
+        graham_a.append(graham_upper_hull[i].x)
+        graham_b.append(graham_upper_hull[i].y)
+
+    gift_a = []
+    gift_b = []
+    for i in range(len(gift_upper_hull)):
+        gift_a.append(gift_upper_hull[i].x)
+        gift_b.append(gift_upper_hull[i].y)    
+
+    chan_a = []
+    chan_b = []
+    for i in range(len(chan_upper_hull)):
+        chan_a.append(chan_upper_hull[i].x)
+        chan_b.append(chan_upper_hull[i].y)          
+
+    
+
+    fig = plt.figure()
+
+    ax1 = fig.add_subplot(131)
+    ax2 = fig.add_subplot(132)
+    ax3 = fig.add_subplot(133)
+    #ax4 = fig.add_subplot(222)
+
+    ax1.scatter(x,y)
+    ax2.scatter(x,y)
+    ax3.scatter(x,y)
+    #ax4.scatter(x,y)
+    
+
+
         
 
-    plt.scatter(x,y) 
-    plt.plot(a, b, color='red', linestyle='dashed', marker='o')
+    #plt.scatter(x,y) 
+    ax1.plot(graham_a, graham_b, color='red', linestyle='dashed', marker='o')
+    ax2.plot(gift_a, gift_b, color='red', linestyle='dashed', marker='o')
+    ax3.plot(chan_a, chan_b, color='red', linestyle='dashed', marker='o')
+
+    ax1.set_title('Graham_scan')
+    ax2.set_title('Gift_wrapping')
+    ax3.set_title('Chan\'s_algorithm')
+
     plt.show()
 
 
 
-def circle_test(algorithm, num_of_points, diameter):
+def circle_test(num_of_points, diameter):
 
 
    # np.random.seed(1)
@@ -50,63 +91,146 @@ def circle_test(algorithm, num_of_points, diameter):
     x = diameter * np.cos(theta)
     y = diameter * np.sin(theta)
 
-    points = []
+    graham_points = []
     for i in range(num_of_points):
         p = Point(x[i],y[i])
-        points.append(p)
+        graham_points.append(p)
         del p
 
-    upper_hull = algorithm(points)
+    gift_points = deepcopy(graham_points)
+    chan_points = deepcopy(graham_points)
+
+
+    graham_upper_hull = grahams_scan(graham_points)
+    gift_upper_hull = gift_wrapping(gift_points)
+    chan_upper_hull = chan_algorithm(chan_points)
 
 
 
-    print_points(upper_hull)
+    #print_points(upper_hull)
 
-    a = []
-    b = []
-    for i in range(len(upper_hull)):
-        a.append(upper_hull[i].x)
-        b.append(upper_hull[i].y)
+    graham_a = []
+    graham_b = []
+    for i in range(len(graham_upper_hull)):
+        graham_a.append(graham_upper_hull[i].x)
+        graham_b.append(graham_upper_hull[i].y)
+
+    gift_a = []
+    gift_b = []
+    for i in range(len(gift_upper_hull)):
+        gift_a.append(gift_upper_hull[i].x)
+        gift_b.append(gift_upper_hull[i].y)    
+
+    chan_a = []
+    chan_b = []
+    for i in range(len(chan_upper_hull)):
+        chan_a.append(chan_upper_hull[i].x)
+        chan_b.append(chan_upper_hull[i].y)          
+
+    
+
+    fig = plt.figure()
+
+    ax1 = fig.add_subplot(131)
+    ax2 = fig.add_subplot(132)
+    ax3 = fig.add_subplot(133)
+    #ax4 = fig.add_subplot(222)
+
+    ax1.scatter(x,y)
+    ax2.scatter(x,y)
+    ax3.scatter(x,y)
+    #ax4.scatter(x,y)
+    
+
+
         
 
-    plt.scatter(x,y) 
-    plt.plot(a, b, color='red', linestyle='dashed', marker='o')
+    #plt.scatter(x,y) 
+    ax1.plot(graham_a, graham_b, color='red', linestyle='dashed', marker='o')
+    ax2.plot(gift_a, gift_b, color='red', linestyle='dashed', marker='o')
+    ax3.plot(chan_a, chan_b, color='red', linestyle='dashed', marker='o')
+
+    ax1.set_title('Graham_scan')
+    ax2.set_title('Gift_wrapping')
+    ax3.set_title('Chan\'s_algorithm')
+
     plt.show()
 
 
 
 
 
-def curve_test(algorithm, num_of_points, range_lower, range_upper):
+def curve_test(num_of_points, range_lower, range_upper):
 
     x = np.random.randint(range_lower,range_upper,num_of_points)
     y = -x**2
 
 
-    points = []
+    graham_points = []
     for i in range(num_of_points):
         p = Point(x[i],y[i])
-        points.append(p)
+        graham_points.append(p)
         del p
 
-    upper_hull = algorithm(points)
+    gift_points = deepcopy(graham_points)
+    chan_points = deepcopy(graham_points)
+
+
+    graham_upper_hull = grahams_scan(graham_points)
+    gift_upper_hull = gift_wrapping(gift_points)
+    chan_upper_hull = chan_algorithm(chan_points)
 
 
 
-    print_points(upper_hull)
+    #print_points(upper_hull)
 
-    a = []
-    b = []
-    for i in range(len(upper_hull)):
-        a.append(upper_hull[i].x)
-        b.append(upper_hull[i].y)
-        
+    graham_a = []
+    graham_b = []
+    for i in range(len(graham_upper_hull)):
+        graham_a.append(graham_upper_hull[i].x)
+        graham_b.append(graham_upper_hull[i].y)
 
-    plt.scatter(x,y) 
-    plt.plot(a, b, color='red', linestyle='dashed', marker='o')
+    gift_a = []
+    gift_b = []
+    for i in range(len(gift_upper_hull)):
+        gift_a.append(gift_upper_hull[i].x)
+        gift_b.append(gift_upper_hull[i].y)    
+
+    chan_a = []
+    chan_b = []
+    for i in range(len(chan_upper_hull)):
+        chan_a.append(chan_upper_hull[i].x)
+        chan_b.append(chan_upper_hull[i].y)          
+
+    
+
+    fig = plt.figure()
+
+    ax1 = fig.add_subplot(131)
+    ax2 = fig.add_subplot(132)
+    ax3 = fig.add_subplot(133)
+    #ax4 = fig.add_subplot(222)
+
+    ax1.scatter(x,y)
+    ax2.scatter(x,y)
+    ax3.scatter(x,y)
+    #ax4.scatter(x,y)
+    
+    #plt.scatter(x,y) 
+    ax1.plot(graham_a, graham_b, color='red', linestyle='dashed', marker='o')
+    ax2.plot(gift_a, gift_b, color='red', linestyle='dashed', marker='o')
+    ax3.plot(chan_a, chan_b, color='red', linestyle='dashed', marker='o')
+
+    ax1.set_title('Graham_scan')
+    ax2.set_title('Gift_wrapping')
+    ax3.set_title('Chan\'s_algorithm')
+
     plt.show()
 
 
 
-square_test(gift_wrapping,568,0,10000)
-#circle_test(chan_algorithm,50000,10000000)
+#square_test(100,0,5000)
+circle_test(200,20000)
+#curve_test(20,0,50)
+
+
