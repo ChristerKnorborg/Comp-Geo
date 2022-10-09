b291f0fa563d4cee395ca7c1271b3273f3ca83c0
Christer <au644764@uni.au.dk>
2022-10-09 13:21:15 +0200

    bugs chan

--- Project2/Chan_algorithm.py
+++ Project2/Chan_algorithm.py
@@ -19,46 +19,46 @@ def binary_search_orientation(arr, p):
     
 
     while low <= high:
-
         mid = (low + high) // 2
 
         # out of range check
-        if mid == 0 or mid == len(arr)-1:
-            return arr[mid]
+        if mid != 0:
+            predecessor = orientation(p, arr[mid], arr[mid-1])
+        else: 
+            predecessor = 1
+        
 
-
-        predecessor = orientation(p, arr[mid], arr[mid-1])
-        successor = orientation(p, arr[mid], arr[mid+1])
+        if mid != len(arr)-1:
+            successor = orientation(p, arr[mid], arr[mid+1])
+        else:
+            successor = 1
+        
 
 
         # If predecessor left turn and successor right turn, split in lower halves.
-        if predecessor == 2 and successor == 1:
+        if predecessor == 2:
             high = mid - 1
 
-        # If predecessor right turn and successor left turn, split in upper halves 
-        elif predecessor == 1 and successor == 2:
+        # If predecessor right turn and successor left turn, split in upper halves.
+        # Same applies if neither predecessor OR successor left turn. But successor is straight.
+        # Here we want 
+        elif successor == 2:
             low = mid + 1
 
-        # If predecessor right turn and successor straight, split in upper halves 
-        # (This is to make only the last point of a straight line be included in upper_hull)
-        #MERGO WITH ABOVE LATER
-        elif predecessor == 1 and successor == 0:
+        elif successor == 0:
             low = mid + 1
 
-        # If predecessor straight and successor right turn, split in lower halves, BUT include current element.
-        # (This is to make the current element be pickable) 
-        #elif predecessor == 0 and successor == 1: 
-            #return arr[mid]
+        # If neither predecessor OR successor left turn. But successor is straight
+        #elif predecessor == 0:
+            #high = mid -1
 
-         # If left turn to neither predec or succ, element is found
-         # predecessor != 2 and successor != 2: PLACEHOLDER ELSE
         else:
             return arr[mid]
-# check punkt længst til venstre eller højre -> terminer!
+
 
 
     # If we reach here, then the element was not present
-    print("end reached")
+    print("Binary Search didn't find optimal")
     return None
 
 
@@ -115,24 +115,25 @@ def upper_hall_with_size(points,h):
 
             for j in range(len(partition_upper_hulls)):
                 
+                # check if upper hull exist for partition
                 if len(partition_upper_hulls[j]) < 1:
                     continue
 
                 else: 
-                    tangent = binary_search_orientation(partition_upper_hulls[j], p)
-                    print("tangent:", (tangent.x,tangent.y))
-                    if tangent != None:
+                    new_tangent = binary_search_orientation(partition_upper_hulls[j], p)
+                    
+                    if new_tangent != None:
                         if best_tangent == None:
-                            best_tangent = tangent
+                            best_tangent = new_tangent
                         else:
-                            if orientation(p, best_tangent, tangent) != 1:
-                                best_tangent = tangent
+                            if orientation(p, best_tangent, new_tangent) != 1:
+                                best_tangent = new_tangent
 
 
 
                 if best_tangent != None:
+                    print("tangent:", (best_tangent.x,best_tangent.y))
                     p = best_tangent
-                    #print("BEST", best_tangent)
             #tangent_start = time.time()
 
             #global tangent_time
@@ -161,26 +162,33 @@ def chan_algorithm(points):
         h = int(2**2**i)
         cond, upper_hull = upper_hall_with_size(points,h)
         if cond:
-            return upper_hull            
+            return upper_hull
+
+    print("CHAN DIDNT FIND UPPER HULL")            
 
 
 p0 = Point(0,0)
-
 p1 = Point(1,1)
 p2 = Point(1,2)
 p3 = Point(2,3)
-p4 = Point(4,4)
-p5 = Point(4,3)
-p6 = Point(6,4)
-p7 = Point(6,2)
-p8 = Point(7,1)
-p9 = Point(8,2)
+p4 = Point(3,4)
+p5 = Point(6,4)
+p6 = Point(8,2)
+p7 = Point(9,1)
 
-points = [p1,p2,p3,p4,p5,p6,p7,p8,p9]
+points = [p0,p1,p2,p3,p4,p5,p6,p7]
 
-#tangent = binary_search_orientation(points,points[1])
+'''
+points = []
+for i in range(0,10):
+    ex = -(i ** 2)
+    p = Point(i,ex)
+    points.append(p)
+    '''
 
-#print(tangent.x,tangent.y)
+
+tangent = binary_search_orientation(points,points[1])
+print(tangent.x,tangent.y)
 
 #start_total = time.time()
 #upper_hull = chan_algorithm(points)
